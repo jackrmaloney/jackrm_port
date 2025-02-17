ccc20e0af63f7dc76c909dbc76818f219544114c
Jack Maloney <jackrmaloney09@gmail.com>
2025-02-17 14:05:03 -0600

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // src/App.tsx
 import { ProfileCard } from './components/profile/ProfileCard';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="bg-[#F9FAFB] px-4 py-8">
       <ProfileCard />
+      <Analytics />
     </div>
   );
 }
